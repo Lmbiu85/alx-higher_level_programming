8267fbe123fd5c3fe94afd5284aeeaa40d977f5f
Lmbiu85 <levymbiu@gmail.com>
2024-05-09 12:29:00 -0700

    4 updated

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL, and displays the body of the response
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "${1}"
